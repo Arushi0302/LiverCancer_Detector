3dd6fb6e9ae5c2ab52ab42b7b4a88d970905ed0e
Anzh <anzhrai@gmail.com>
2021-03-04 20:59:12 +0530

    API Added

--- app/main.py
+++ app/main.py
@@ -12,7 +12,11 @@ CORS(app)
 #Prediction Funvtion
 model = pickle.load(open('models/liver.pkl','rb'))
 def predict(values, dic):
+    if len(values) > 10 or len(values) < 10:
+        return -1
+
     values = np.asarray(values)
+
     return model.predict(values.reshape(1, -1))[0]
 
 #Home Route
@@ -38,6 +42,24 @@ def predictPage():
         return render_template("home.html", message = message)
 
     return render_template('predict.html', pred = pred)
+
+@app.route("/livercancer-API", methods = ['POST', 'GET'])
+def api_pred():
+    pred = -1
+
+    try:
+        if request.method == 'POST':
+            to_predict_dict = request.get_json()
+            to_predict_list = list(map(float, list(to_predict_dict.values())))
+            pred = predict(to_predict_list, to_predict_dict)
+        else:
+            to_predict_dict = request.args
+            to_predict_list = list(map(float, list(to_predict_dict.values())))
+            pred = predict(to_predict_list, to_predict_dict)
+    except:
+        return -1, 200
+
+    return pred, 200
 
 if __name__ == '__main__':
 	app.run(debug = True)
